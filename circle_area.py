821a4fe8a5565b7c50edad78a65e4d6e76c4c2be
Meshack Nyagwencha <meshagwe@gmail.com>
2023-03-05 08:47:06 +0300

    commit one

---
+++ circle_area.py
@@ -0,0 +1,18 @@
+def circle_area(diameter = 3):
+    '''A function that calculates the area of a circle given the diameter'''
+    pi = 3.14159 # approximate
+    
+    # TODO:
+    # Create a variable called 'radius' equal to half the diameter
+    # Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+    # Be sure to return the 'area'
+
+    # YOUR CODE HERE
+    radius = diameter/2
+    area = pi*radius**2
+
+    
+    print(area)
+def main():
+    circle_area(10)
+main()
---
+++ hello.py
@@ -0,0 +1,33 @@
+
+
+fname = "Meshack"
+
+fname_length=len(fname)
+
+print(fname_length)
+for i in range(0, fname_length):
+    print(i)
+e = ['home', 'away', 'from']
+
+
+
+
+
+
+str1='I think therefore I am'
+
+str2=str1[-4:]
+str3=str1[7:-4]
+print(str1[2:8]*3)
+print(str2+str3+str1[:7])
+
+mixed = [1,2.3,5,6,['Meshack','Math']]
+print(mixed[4][-1])
+
+string1 = 'stress'
+res = ''
+sum = 1
+for counter in string1:
+    counter = res
+    print(res)
+
